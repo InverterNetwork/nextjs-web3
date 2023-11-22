acee775287e81189804d531712f5c051c087c04c
light-co-turkey <light.co.turkey@gmail.com>
2023-11-22 18:42:41 +0300

    feat: tanstack query

--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -1,22 +1,27 @@
 'use client'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReduxProvider from '../lib/store/ReduxProvider'
 import ConnectorProvider from './ConnectorProvider'
 import AppProvider from './appContext'
 import { theme } from '@/lib'
 import { ChakraProvider, cookieStorageManager } from '@chakra-ui/react'
 
+const queryClient = new QueryClient()
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider theme={theme} colorModeManager={cookieStorageManager}>
-      <ReduxProvider>
-        <ConnectorProvider>
-          <AppProvider>
-            {/* STYLE PROVIDERS AND CHILDREN */}
-            {children}
-          </AppProvider>
-        </ConnectorProvider>
-      </ReduxProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReduxProvider>
+          <ConnectorProvider>
+            <AppProvider>
+              {/* STYLE PROVIDERS AND CHILDREN */}
+              {children}
+            </AppProvider>
+          </ConnectorProvider>
+        </ReduxProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
