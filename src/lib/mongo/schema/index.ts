ab1da05a5594606afea4530f8f47d1ab2cfd871a
mguleryuz <mguleryuz3@gmail.com>
2024-10-03 10:49:44 -0500

    fix: filenames

--- src/lib/mongo/schema/index.ts
+++ src/lib/mongo/schema/index.ts
@@ -1,7 +1,7 @@
 import { ApiSecret } from './api-secret'
-import { Cache } from './_cache'
+import { Cache } from './cache'
 import { EventCache } from './event-cache'
-import { User } from './_user'
+import { User } from './user'
 
 export default {
   User,
